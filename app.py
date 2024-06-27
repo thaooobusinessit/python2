d7af95022102124537abfa97d9e9dd10da0a70c8
thaooobusinessit <10623039@student.vgu.edu.vn>
2024-06-27 16:57:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ with tab1:
         
     fig1b = px.line(df, x="weekly_self_study_hours", y="math_score", 
                 labels={"weekly_self_study_hours": "Weekly Self-study Hours", "math_score": "Math Score"},
-                color=by_what, facet_col=by_what, facet_col_wrap=3)
+                color=by_what, facet_col_wrap=3)
     st.plotly_chart(fig1b, theme="streamlit", use_container_width=False, height=800)
 with tab2:
     category_mapping = {
